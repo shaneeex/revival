0d4b319ca2fe42e8fd370c2d3da82e34c5451159
shaneeex <shaneeex@users.noreply.github.com>
2026-03-14 23:50:38 +0500

    Remove powered-by label and enlarge partner logos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "revival-signage-v35";
+const CACHE_NAME = "revival-signage-v36";
 const PRECACHE_URLS = [
   "./",
   "./index.html",
@@ -12,7 +12,7 @@ const PRECACHE_URLS = [
   "./admin.js?v=20260314l",
   "./runtime-config.json",
   "./styles.css",
-  "./styles.css?v=20260314j",
+  "./styles.css?v=20260314m",
   "./script.js",
   "./script.js?v=20260314j",
   "./manifest.webmanifest"
